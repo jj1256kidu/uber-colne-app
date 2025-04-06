4ccf43a8c79fa8b77be3fa7b1be45fdba47e3fb3
jj1256kidu <tourist2towine@gmail.com>
2025-04-06 16:36:37 +0530

    Update locations.py

--- data/locations.py
+++ data/locations.py
@@ -1,3 +1,4 @@
+# List of popular locations for the demo
 popular_locations = [
     "Times Square, New York",
     "Central Park, New York",
@@ -9,4 +10,30 @@ popular_locations = [
     "Metropolitan Museum of Art, New York",
     "Rockefeller Center, New York",
     "One World Trade Center, New York"
+] 
+
+saved_locations = [
+    {
+        'name': 'Home',
+        'address': '123 Main St, New York, NY'
+    },
+    {
+        'name': 'Work',
+        'address': '1455 Market St, New York, NY'
+    }
+]
+
+recent_locations = [
+    {
+        'name': 'Central Park',
+        'address': 'Central Park, New York, NY'
+    },
+    {
+        'name': 'Times Square',
+        'address': 'Times Square, Manhattan, NY'
+    },
+    {
+        'name': 'Brooklyn Bridge',
+        'address': 'Brooklyn Bridge, New York, NY'
+    }
 ] 
