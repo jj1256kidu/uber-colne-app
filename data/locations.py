4b2acc962c551b430abace339bde64ec3682f7e6
jj1256kidu <tourist2towine@gmail.com>
2025-04-06 16:13:12 +0530

    Create locations.py

---
+++ data/locations.py
@@ -0,0 +1,12 @@
+popular_locations = [
+    "Times Square, New York",
+    "Central Park, New York",
+    "Empire State Building, New York",
+    "Brooklyn Bridge, New York",
+    "Grand Central Terminal, New York",
+    "Statue of Liberty, New York",
+    "Madison Square Garden, New York",
+    "Metropolitan Museum of Art, New York",
+    "Rockefeller Center, New York",
+    "One World Trade Center, New York"
+] 
