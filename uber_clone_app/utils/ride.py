365c846b99a716ef11adfad57e4fcfb6793542d1
jj1256kidu <tourist2towine@gmail.com>
2025-04-06 17:04:40 +0530

    Create ride.py

---
+++ uber_clone_app/utils/ride.py
@@ -0,0 +1,10 @@
+import random
+
+MOCK_DRIVERS = [
+    {
+        'name': 'John Smith',
+        'photo': 'https://randomuser.me/api/portraits/men/1.jpg',
+        'car': 'Toyota Camry',
+        'plate': 'ABC123',
+    }
+] 
