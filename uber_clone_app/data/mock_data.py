14084d2bafbc1cce6a07d4417d003ae0d70bae8e
jj1256kidu <tourist2towine@gmail.com>
2025-04-06 17:06:26 +0530

    Create mock_data.py

---
+++ uber_clone_app/data/mock_data.py
@@ -0,0 +1,14 @@
+SAVED_LOCATIONS = [
+    {
+        'name': 'Work',
+        'address': '1455 Market Street',
+        'coordinates': [40.7128, -74.0060],
+        'icon': '🏢'
+    },
+    {
+        'name': 'Home',
+        'address': '1600 Michigan Street',
+        'coordinates': [40.7589, -73.9851],
+        'icon': '🏠'
+    }
+] 
